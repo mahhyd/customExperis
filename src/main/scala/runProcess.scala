599cb9627622fe0e1e709b547fbad7a9b04847f6
mahfoud <maitmohamed@novagen.tech>
2019-04-19 10:54:18 +0200

    Dataframe and DataSet

--- src/main/scala/runProcess.scala
+++ src/main/scala/runProcess.scala
@@ -128,8 +128,55 @@ object runProcess extends App {
   //spark.sql("select * from globalViewProduct").show(14)
   //spark.newSession().sql("select * from globalViewProduct").show(16)
 
-  //
-
 
   //************************* third Part (use case class for convert DF to DS) *******************************
+  //load region.csv with schema
+  val regionSchemattt = StructType(Array(
+    StructField("region_id", IntegerType, true),
+    StructField("sales_city", StringType, true),
+    StructField("sales_state_province", StringType, true),
+    StructField("sales_district", StringType, true),
+    StructField("sales_region", StringType, true),
+    StructField("sales_country", StringType, true),
+    StructField("sales_district_id", IntegerType, true)))
+  // 1.Create DataSet from StructType
+  import spark.implicits._
+  // Note: Case classes in Scala 2.10 can support only up to 22 fields. To work around this limit,
+  // you can use custom classes that implement the Product interface
+  case class Region(region_id: Int, sales_city: String, sales_state_province: String, sales_district: String, sales_region: String, sales_country: String, sales_district_id: Int)
+  // Encoders are created for case classes
+  val caseClassRegionDS1 = Seq(Region(1, "San Francisco", "CA", "San Francisco", "Central West", "USA", 123)).toDS()
+  caseClassRegionDS1.show()
+
+  // 2.Create DataSet from file using case class
+  val caseClassRegionDS2 = dfRegion.as[Region]
+  caseClassRegionDS2.show(16)
+
+  // 3.Create DataSet from text file structured
+  val dfRegiontxt = spark.read
+    .format("csv")
+    .option("header", "false")
+    .option("delimiter", ";")
+    .schema(regionSchema)
+    .load("src/main/ressource/region.txt")
+  dfRegiontxt.show(10)
+
+  // 3.Create DataSet from text file not structured using case class
+  val dfRegiontxt1 = spark.read.textFile("src/main/ressource/region.txt").map(x => x.split(";")).show(11)
+
+  val dfRegiontxt2 = spark.read.textFile("src/main/ressource/region.txt").map(x => x.split(";")).map(arrays => Region(arrays(0).toInt, arrays(1).toString, arrays(2).toString, arrays(3).toString, arrays(4).toString, arrays(5).toString, arrays(6).toInt))
+  dfRegiontxt2.show(9)
+
+  // 4.Create DataSet from rdd
+  val rddRegion = spark.sparkContext.textFile("src/main/ressource/region.txt")
+  //rddRegion.collect().map(line => println(line))
+
+  // 4.1.Create DataSet from rdd using case class
+  val dfRegion1 = rddRegion.map(_.split(";")).map(arrays => Region(arrays(0).toInt, arrays(1).toString, arrays(2).toString, arrays(3).toString, arrays(4).toString, arrays(5).toString, arrays(6).toInt)).toDS()
+  dfRegion1.show(17)
+
+  // 4.2. Create DataSet from rdd using StructureType
+
+
+
 }
